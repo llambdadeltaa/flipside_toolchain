926a535bf3e1778b6d6bbd57251e275846c50e91
llambdadeltaa <amrest.c@gmail.com>
2022-01-16 16:11:19 +0700

    Add sort option

--- src/flipside_toolchain/terra/velocity_api.py
+++ src/flipside_toolchain/terra/velocity_api.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-def read_dataframe(url: str, time_column: str = 'DT', utc_time: bool = True):
+def read_dataframe(url: str, time_column: str = 'DT', utc_time: bool = True, sort_date: bool = True):
     df = pd.read_json(url)
-    df.loc[time_column] = pd.to_datetime(df[time_column], utc=True)
+    df.loc[time_column] = pd.to_datetime(df[time_column], utc=utc_time)
+    df.sort_values(by=time_column, ascending=False, inplace=True)
     return df
